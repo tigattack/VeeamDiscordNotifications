af0548b3913156aca4bb686f3db2f8ec926711b6
Daniel Thorpe <daniel.thorpe@citrix.com>
2019-02-06 14:14:05 +0000

    Fix a mistake where config would be overwritten on update

--- UpdateVeeamDiscordNotification.ps1
+++ UpdateVeeamDiscordNotification.ps1
@@ -1,12 +1,33 @@
 ﻿Param (
     [string]$LatestVersion
     )
+$currentConfig = (Get-Content "$PSScriptRoot\config\conf.json") -Join "`n" | ConvertFrom-Json
 Invoke-WebRequest -Uri https://github.com/tigattack/VeeamDiscordNotifications/releases/download/$LatestVersion/VeeamDiscordNotifications-$LatestVersion.zip -OutFile C:\VeeamScripts\VeeamDiscordNotifications-$LatestVersion.zip
 Expand-Archive C:\VeeamScripts\VeeamDiscordNotifications-$LatestVersion.zip
 Remove-Item C:\VeeamScripts\VeeamDiscordNotifications -Recurse -Force
 Rename-Item C:\VeeamScripts\VeeamDiscordNotifications-$LatestVersion C:\VeeamScripts\VeeamDiscordNotifications
 Remove-Item C:\VeeamScripts\VeeamDiscordNotifications-$LatestVersion.zip
+$newConfig = (Get-Content "$PSScriptRoot\config\conf.json") -Join "`n" | ConvertFrom-Json
 Unblock-File C:\VeeamScripts\VeeamDiscordNotifications\DiscordNotificationBootstrap.ps1
 Unblock-File C:\VeeamScripts\VeeamDiscordNotifications\DiscordVeeamAlertSender.ps1
 Unblock-File C:\VeeamScripts\VeeamDiscordNotifications\resources\logger.psm1
 Unblock-File C:\VeeamScripts\VeeamDiscordNotifications\UpdateVeeamDiscordNotification.ps1
+if ($currentConfig.webhook -ne $newConfig.webhook) {
+    $newConfig.webhook = $currentConfig.webhook
+}
+if ($currentConfig.thumbnail -ne $newConfig.thumbnail) {
+    $newConfig.thumbnail = $currentConfig.thumbnail
+}
+if ($currentConfig.userid -ne $newConfig.userid) {
+    $newConfig.userid = $currentConfig.userid
+}
+if ($currentConfig.mention_on_fail -ne $newConfig.mention_on_fail) {
+    $newConfig.mention_on_fail = $currentConfig.mention_on_fail
+}
+if ($currentConfig.debug_log -ne $newConfig.debug_log) {
+    $newConfig.debug_log = $currentConfig.debug_log
+}
+if ($currentConfig.auto_update -ne $newConfig.auto_update) {
+    $newConfig.auto_update = $currentConfig.auto_update
+}
+ConvertTo-Json $newConfig | Set-Content "$PSScriptRoot\config\conf.json"
