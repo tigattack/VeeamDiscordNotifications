3d88b3b188c313dacf896a773fcab8b91f4602d4
Daniel Thorpe <daniel.thorpe@citrix.com>
2019-02-06 13:57:24 +0000

    Completed update processing Added config option to enable auto-update, default true

--- UpdateVeeamDiscordNotification.ps1
+++ UpdateVeeamDiscordNotification.ps1
@@ -1,3 +1,12 @@
 ﻿Param (
-    [string]$latestVersion
+    [string]$LatestVersion
     )
+Invoke-WebRequest -Uri https://github.com/tigattack/VeeamDiscordNotifications/releases/download/$LatestVersion/VeeamDiscordNotifications-$LatestVersion.zip -OutFile C:\VeeamScripts\VeeamDiscordNotifications-$LatestVersion.zip
+Expand-Archive C:\VeeamScripts\VeeamDiscordNotifications-$LatestVersion.zip
+Remove-Item C:\VeeamScripts\VeeamDiscordNotifications -Recurse -Force
+Rename-Item C:\VeeamScripts\VeeamDiscordNotifications-$LatestVersion C:\VeeamScripts\VeeamDiscordNotifications
+Remove-Item C:\VeeamScripts\VeeamDiscordNotifications-$LatestVersion.zip
+Unblock-File C:\VeeamScripts\VeeamDiscordNotifications\DiscordNotificationBootstrap.ps1
+Unblock-File C:\VeeamScripts\VeeamDiscordNotifications\DiscordVeeamAlertSender.ps1
+Unblock-File C:\VeeamScripts\VeeamDiscordNotifications\resources\logger.psm1
+Unblock-File C:\VeeamScripts\VeeamDiscordNotifications\UpdateVeeamDiscordNotification.ps1
