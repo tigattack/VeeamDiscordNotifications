bd49b60aadabff82111db2606bca90e5867b1335
tigattack <10629864+tigattack@users.noreply.github.com>
2019-02-07 10:44:50 +0000

    Converted static paths, added logging & comments, set timeout
    > Converted static paths to $PSScriptRoot. This fixes any issues that could have arisen from people having the script in a different location than advised.
    > Added logging
    > Added comments
    > Set a timeout for the script where it will quit if the alert sender (which triggers is) is still running while this is trying to update the project.

--- UpdateVeeamDiscordNotification.ps1
+++ UpdateVeeamDiscordNotification.ps1
@@ -1,20 +1,40 @@
-﻿Param (
+# Pull version from script trigger
+Param (
     [string]$LatestVersion
-    )
-$currentConfig = (Get-Content "C:\VeeamScripts\VeeamDiscordNotifications\config\conf.json") -Join "`n" | ConvertFrom-Json
-Invoke-WebRequest -Uri https://github.com/tigattack/VeeamDiscordNotifications/releases/download/$LatestVersion/VeeamDiscordNotifications-$LatestVersion.zip -OutFile C:\VeeamScripts\VeeamDiscordNotifications-$LatestVersion.zip
+)
+# Import functions
+Import-Module "$PSScriptRoot\VeeamDiscordNotifications\resources\logger.psm1"
+# Start logging
+Start-Logging "$PSScriptRoot\VeeamDiscordNotifications\log\update.log"
+
+# Pull current config to variable
+$currentConfig = (Get-Content "$PSScriptRoot\VeeamDiscordNotifications\config\conf.json") -Join "`n" | ConvertFrom-Json
+
+# Wait until the alert sender has finished running, or quit this if it's still running after 60s. It should never take that long.
 while (Get-WmiObject win32_process -filter "name='powershell.exe' and commandline like '%DiscordVeeamAlertSender.ps1%'") {
+    $timer++
     Start-Sleep -Seconds 1
+    If ($timer -eq '60') {
+        Write-Output 'Timeout reached. Updater quitting as DiscordVeeamAlertSender.ps1 is still running after 60 seconds.'
+        exit
+    }
 }
-Expand-Archive C:\VeeamScripts\VeeamDiscordNotifications-$LatestVersion.zip -DestinationPath C:\VeeamScripts
-Remove-Item C:\VeeamScripts\VeeamDiscordNotifications -Recurse -Force
-Rename-Item C:\VeeamScripts\VeeamDiscordNotifications-$LatestVersion C:\VeeamScripts\VeeamDiscordNotifications
-Remove-Item C:\VeeamScripts\VeeamDiscordNotifications-$LatestVersion.zip
-$newConfig = (Get-Content "C:\VeeamScripts\VeeamDiscordNotifications\config\conf.json") -Join "`n" | ConvertFrom-Json
-Unblock-File C:\VeeamScripts\VeeamDiscordNotifications\DiscordNotificationBootstrap.ps1
-Unblock-File C:\VeeamScripts\VeeamDiscordNotifications\DiscordVeeamAlertSender.ps1
-Unblock-File C:\VeeamScripts\VeeamDiscordNotifications\resources\logger.psm1
-Unblock-File C:\VeeamScripts\VeeamDiscordNotifications\UpdateVeeamDiscordNotification.ps1
+# Pull latest version of script from GitHub
+Invoke-WebRequest -Uri https://github.com/tigattack/VeeamDiscordNotifications/releases/download/$LatestVersion/VeeamDiscordNotifications-$LatestVersion.zip -OutFile $PSScriptRoot\VeeamDiscordNotifications-$LatestVersion.zip
+# Expand downloaded ZIP
+Expand-Archive $PSScriptRoot\VeeamDiscordNotifications-$LatestVersion.zip -DestinationPath $PSScriptRoot
+# Remove old version except log files.
+Get-ChildItem $PSScriptRoot\VeeamDiscordNotifications\ -Include *.* -Exclude '*.log*', -File -Recurse | Remove-Item -Force
+# Rename extracted update
+Rename-Item $PSScriptRoot\VeeamDiscordNotifications-$LatestVersion $PSScriptRoot\VeeamDiscordNotifications
+# Pull configuration from new conf file
+$newConfig = (Get-Content "$PSScriptRoot\VeeamDiscordNotifications\config\conf.json") -Join "`n" | ConvertFrom-Json
+# Unblock script files
+Unblock-File $PSScriptRoot\VeeamDiscordNotifications\DiscordNotificationBootstrap.ps1
+Unblock-File $PSScriptRoot\VeeamDiscordNotifications\DiscordVeeamAlertSender.ps1
+Unblock-File $PSScriptRoot\VeeamDiscordNotifications\resources\logger.psm1
+Unblock-File $PSScriptRoot\VeeamDiscordNotifications\UpdateVeeamDiscordNotification.ps1
+# Set conf.json as it was before
 $newConfig.webhook = $currentConfig.webhook
 $newConfig.userid = $currentConfig.userid
 if ($currentConfig.mention_on_fail -ne $newConfig.mention_on_fail) {
@@ -26,5 +46,10 @@ if ($currentConfig.debug_log -ne $newConfig.debug_log) {
 if ($currentConfig.auto_update -ne $newConfig.auto_update) {
     $newConfig.auto_update = $currentConfig.auto_update
 }
-ConvertTo-Json $newConfig | Set-Content "C:\VeeamScripts\VeeamDiscordNotifications\config\conf.json"
+ConvertTo-Json $newConfig | Set-Content "$PSScriptRoot\VeeamDiscordNotifications\config\conf.json"
+
+# Clean up.
+Remove-Item "$PSScriptRoot\VeeamDiscordNotifications-$LatestVersion.zip"
 Remove-Item -LiteralPath $MyInvocation.MyCommand.Path -Force
+# Stop logging
+Stop-Logging "$PSScriptRoot\VeeamDiscordNotifications\log\update.log"
