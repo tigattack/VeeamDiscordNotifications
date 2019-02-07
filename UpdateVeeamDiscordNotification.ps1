01178b28e43b4784e744882ce13766ca759ebcf7
tigattack <10629864+tigattack@users.noreply.github.com>
2019-02-07 22:38:20 +0000

    Update notifications and failure recovery
    > Notify upon update with result, new version, and old version.
    > Rename old version and delete if successful, or put it back in place if unsuccessful (instead of deleting the old version before updating). Failure detection is very basic at this point and should be improved.

--- UpdateVeeamDiscordNotification.ps1
+++ UpdateVeeamDiscordNotification.ps1
@@ -2,6 +2,54 @@
 Param (
     [string]$LatestVersion
 )
+# Notification script block
+$notification = {
+    # Create embed and fields array
+    [System.Collections.ArrayList]$embedarray = @()
+    [System.Collections.ArrayList]$fieldarray = @()
+    # Thumbnail object
+    $thumbobject = [PSCustomObject]@{
+	    url = $currentconfig.thumbnail
+    }
+    # Field objects
+    $resultfield = [PSCustomObject]@{
+	    name = 'Update Result'
+        value = $result
+        inline = 'false'
+    }
+    $newversionfield = [PSCustomObject]@{
+	    name = 'New version'
+        value = $newversion
+        inline = 'false'
+    }
+    $oldversionfield = [PSCustomObject]@{
+	    name = 'Old version'
+        value = $oldversion
+        inline = 'false'
+    }
+    # Add field objects to the field array
+    $fieldarray.Add($oldversionfield) | Out-Null
+    $fieldarray.Add($newversionfield) | Out-Null
+    $fieldarray.Add($resultfield) | Out-Null
+    # Embed object including field and thumbnail vars from above
+    $embedobject = [PSCustomObject]@{
+        title		= $JobName
+        color		= '1267393'
+        thumbnail	= $thumbobject
+        fields		= $fieldarray
+    }
+    # Add embed object to the array created above
+    $embedarray.Add($embedobject) | Out-Null
+    # Build payload
+    $payload = [PSCustomObject]@{
+    	embeds	= $embedarray
+    }
+    # Send iiit
+    Invoke-RestMethod -Uri $currentconfig.webhook -Body ($payload | ConvertTo-Json -Depth 4) -Method Post -ContentType 'application/json'
+}
+
+# Get currently downloaded version
+$oldversion = Get-Content "$PSScriptRoot\VeeamDiscordNotifications\resources\version.txt"
 # Import functions
 Import-Module "$PSScriptRoot\VeeamDiscordNotifications\resources\logger.psm1"
 # Start logging
@@ -23,8 +71,8 @@ while (Get-WmiObject win32_process -filter "name='powershell.exe' and commandlin
 Invoke-WebRequest -Uri https://github.com/tigattack/VeeamDiscordNotifications/releases/download/$LatestVersion/VeeamDiscordNotifications-$LatestVersion.zip -OutFile $PSScriptRoot\VeeamDiscordNotifications-$LatestVersion.zip
 # Expand downloaded ZIP
 Expand-Archive $PSScriptRoot\VeeamDiscordNotifications-$LatestVersion.zip -DestinationPath $PSScriptRoot
-# Remove old version except log files.
-Get-ChildItem $PSScriptRoot\VeeamDiscordNotifications\ -Include *.* -Exclude '*.log*', -File -Recurse | Remove-Item -Force
+# Rename old version to make room for the new version
+Rename-Item $PSScriptRoot\VeeamDiscordNotifications $PSScriptRoot\VeeamDiscordNotifications-old
 # Rename extracted update
 Rename-Item $PSScriptRoot\VeeamDiscordNotifications-$LatestVersion $PSScriptRoot\VeeamDiscordNotifications
 # Pull configuration from new conf file
@@ -48,8 +96,27 @@ if ($currentConfig.auto_update -ne $newConfig.auto_update) {
 }
 ConvertTo-Json $newConfig | Set-Content "$PSScriptRoot\VeeamDiscordNotifications\config\conf.json"
 
+# Get newly downloaded version
+$newversion = Get-Content "$PSScriptRoot\VeeamDiscordNotifications\resources\version.txt"
+
+# Send notification
+If ($newversion -eq $LatestVersion) {
+    $result = 'Success!'
+    Remove-Item –Path $PSScriptRoot\VeeamDiscordNotifications-old –Recurse -Force
+    Write-Output 'Successfully updated.'
+    Invoke-Command -ScriptBlock $notification
+}
+Else {
+    $result = 'Failure!'
+    Remove-Item –Path $PSScriptRoot\VeeamDiscordNotifications –Recurse -Force
+    Rename-Item $PSScriptRoot\VeeamDiscordNotifications-old $PSScriptRoot\VeeamDiscordNotifications
+    Write-Output 'Update failed, reverted to previous version.'
+    Invoke-Command -ScriptBlock $notification
+}
+
 # Clean up.
 Remove-Item "$PSScriptRoot\VeeamDiscordNotifications-$LatestVersion.zip"
 Remove-Item -LiteralPath $MyInvocation.MyCommand.Path -Force
+
 # Stop logging
 Stop-Logging "$PSScriptRoot\VeeamDiscordNotifications\log\update.log"
