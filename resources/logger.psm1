cb2a64bb887d0ef8de732195cf89b6f33d44d2f1
tigattack <10629864+tigattack@users.noreply.github.com>
2020-06-04 10:51:50 +0100

    Convert spaces to tabs in logger function.

--- resources/logger.psm1
+++ resources/logger.psm1
@@ -1,18 +1,18 @@
 # This function logs messages with a type tag
 function Write-LogMessage($tag, $message) {
-    Write-Host "[$tag] $message"
+	Write-Host "[$tag] $message"
 }
 
 # This function handles Logging
 function Start-Logging($path) {
-    try {
-        Start-Transcript -path $path -force -append 
-        Write-LogMessage -Tag 'Info' -Message "Transcript is being logged to $path"
-    } catch [Exception] {
-        Write-LogMessage -Tag 'Info' -Message "Transcript is already being logged to $path"
-    }
+	try {
+		Start-Transcript -path $path -force -append
+		Write-LogMessage -Tag 'Info' -Message "Transcript is being logged to $path"
+	} catch [Exception] {
+		Write-LogMessage -Tag 'Info' -Message "Transcript is already being logged to $path"
+	}
 }
 function Stop-Logging {
-    Write-LogMessage -Tag 'Info' -Message "Stopping transcript logging."
-    Stop-Transcript
+	Write-LogMessage -Tag 'Info' -Message "Stopping transcript logging."
+	Stop-Transcript
 }
