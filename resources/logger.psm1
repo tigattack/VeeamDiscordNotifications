1cde5c253a066c7f7caab63e4c2971c97a4e8410
tigattack <10629864+tigattack@users.noreply.github.com>
2021-03-18 10:32:49 +0000

    fix(logger): PSUseShouldProcessForStateChangingFunctions

--- resources/logger.psm1
+++ resources/logger.psm1
@@ -1,18 +1,46 @@
 # This function logs messages with a type tag
-function Write-LogMessage($tag, $message) {
-	Write-Output "[$tag] $message"
-}
-
-# This function handles Logging
-function Start-Logging($path) {
-	try {
-		Start-Transcript -path $path -force -append
-		Write-LogMessage -Tag 'Info' -Message "Transcript is being logged to $path"
-	} catch [Exception] {
-		Write-LogMessage -Tag 'Info' -Message "Transcript is already being logged to $path"
+Function Write-LogMessage {
+	[CmdletBinding(
+		SupportsShouldProcess,
+		ConfirmImpact = 'Low'
+	)]
+	Param (
+		$tag,
+		$message
+	)
+	If ($PSCmdlet.ShouldProcess('Output stream', 'Write log message')) {
+		Write-Output "[$tag] $message"
 	}
 }
-function Stop-Logging {
-	Write-LogMessage -Tag 'Info' -Message "Stopping transcript logging."
-	Stop-Transcript
+
+# These functions handles Logging
+Function Start-Logging {
+	[CmdletBinding(
+		SupportsShouldProcess,
+		ConfirmImpact = 'Low'
+	)]
+	Param(
+		[Parameter(Mandatory)]
+		$path
+	)
+	If ($PSCmdlet.ShouldProcess($path, 'Start-Transcript')) {
+		Try {
+			Start-Transcript -Path $path -Force -Append
+			Write-LogMessage -Tag 'Info' -Message "Transcript is being logged to $path"
+		}
+		Catch [Exception] {
+			Write-LogMessage -Tag 'Info' -Message "Transcript is already being logged to $path"
+		}
+	}
+}
+Function Stop-Logging {
+	[CmdletBinding(
+		SupportsShouldProcess,
+		ConfirmImpact = 'Low'
+	)]
+	Param()
+	If ($PSCmdlet.ShouldProcess('log file', 'Stop-Transcript')) {
+		Write-LogMessage -Tag 'Info' -Message "Stopping transcript logging."
+		Stop-Transcript
+	}
 }
