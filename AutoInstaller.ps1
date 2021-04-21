59cc6d5c38d4a41126e6f3354da0e1500d8c39ce
s0yun <77540086+s0yun@users.noreply.github.com>
2021-04-21 09:35:25 +0100

    Update to Auto Installer

--- AutoInstaller.ps1
+++ AutoInstaller.ps1
@@ -1,33 +1,36 @@
-
-
 # Check user has webhook URL ready
 $userPrompt = Read-Host -Prompt "Do you have your webhook URL ready? Y/N"
 if ($userprompt -ne 'Y') {
     Write-Output "Please create a Discord webhook before continuing."
     exit
 }
-    #Create Directory structure
-    New-Item C:\VeeamScripts -Type directory
-    $downloadlocation = Read-Host -Prompt "Please Type the full path of your download location including Drive letter"
-    Expand-Archive $downloadlocation -DestinationPath C:\VeeamScripts
-    Rename-Item C:\VeeamScripts\VeeamDiscordNotifications-v1.5 C:\VeeamScripts\VeeamDiscordNotifications
+#Create Directory structure
+New-Item C:\VeeamScripts -Type directory# Get latest release from GitHub.
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$latestRelease = Invoke-WebRequest -Uri https://github.com/tigattack/VeeamDiscordNotifications/releases/latest -Headers @{"Accept"="application/json"} -UseBasicParsing
+# Release IDs are returned in a format of {"id":3622206,"tag_name":"v1.0"} so we need to extract tag_name.
+$latestVersion = $latestRelease.Content | ConvertFrom-Json | Select-Object tag_name -ExpandProperty tag_name
+# Pull latest version of script from GitHub
+Invoke-WebRequest -Uri https://github.com/tigattack/VeeamDiscordNotifications/releases/download/$LatestVersion/VeeamDiscordNotifications-$LatestVersion.zip -OutFile $PSScriptRoot\VeeamDiscordNotifications-$LatestVersion.zip
+# Expand downloaded ZIP
+Expand-Archive $PSScriptRoot\VeeamDiscordNotifications-$LatestVersion.zip -DestinationPath C:\VeeamScripts
 
 #Assign webhook url to variable
 $webhookurl = Read-Host -Prompt "Please paste your Webhook URL now"
 
-#Read Config File & Write the user webhook
+#Get the config file and write the user webhook
 $Config = (Get-Content C:\VeeamScripts\VeeamDiscordNotifications\config\conf.json) | ConvertFrom-Json
 $Config.webhook = $webhookurl
 #Write Config
 ConvertTo-Json $Config | Set-Content C:\VeeamScripts\VeeamDiscordNotifications\config\conf.json
- # Unblock script files
- Unblock-File C:\VeeamScripts\VeeamDiscordNotifications\DiscordNotificationBootstrap.ps1
- Unblock-File C:\VeeamScripts\VeeamDiscordNotifications\DiscordVeeamAlertSender.ps1
+#Unblock script files
+Unblock-File C:\VeeamScripts\VeeamDiscordNotifications\DiscordNotificationBootstrap.ps1
+Unblock-File C:\VeeamScripts\VeeamDiscordNotifications\DiscordVeeamAlertSender.ps1
 Unblock-File C:\VeeamScripts\VeeamDiscordNotifications\resources\logger.psm1
 Unblock-File C:\VeeamScripts\VeeamDiscordNotifications\UpdateVeeamDiscordNotification.ps1
 
 
 #Display the command for Veeam
-
-Write-Host "Success. Copy the following command into the following area of each job you would like to have reported.`nJob settings -> Storage -> Advanced -> Scripts -> Post-Job Script"
-Write-Host "Powershell.exe -ExecutionPolicy Bypass -File C:\VeeamScripts\VeeamDiscordNotifications\DiscordNotificationBootstrap.ps1"
+Write-Output "Success. Copy the following command into the following area of each job you would like to have reported."
+Write-Output "`Job settings -> Storage -> Advanced -> Scripts -> Post-Job Script"
+Write-Output "Powershell.exe -ExecutionPolicy Bypass -File C:\VeeamScripts\VeeamDiscordNotifications\DiscordNotificationBootstrap.ps1"
