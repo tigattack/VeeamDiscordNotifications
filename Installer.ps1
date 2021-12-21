3d0b14a6da9c5360f85aec83f42d5d30313217e4
tigattack <10629864+tigattack@users.noreply.github.com>
2021-12-21 18:20:50 +0000

    fix(installer): config properties not found
    affects older releases and current latest stable

--- Installer.ps1
+++ Installer.ps1
@@ -4,6 +4,7 @@
 $rootPath = 'C:\VeeamScripts'
 $project = 'VeeamDiscordNotifications'
 $webhookRegex = 'https:\/\/(.*\.)?discord(app)?\.com\/api\/webhooks\/([^\/]+)\/([^\/]+)'
+$mentionOnWarnExist = $mentionOnFailExist = $true
 
 # Get latest release from GitHub
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
@@ -51,7 +52,7 @@ else {
 if (Test-Path $rootPath\$project) {
 	$installedVersion = Get-Content -Raw "$rootPath\$project\resources\version.txt"
 	If ($installedVersion -ge $release) {
-		Write-Output "VeeamDiscordNotifications is already installed and up to date.`nExiting."
+		Write-Output "`nVeeamDiscordNotifications is already installed and up to date.`nExiting."
 		Start-Sleep -Seconds 5
 		exit
 	}
@@ -59,57 +60,6 @@ if (Test-Path $rootPath\$project) {
 		Write-Output "VeeamDiscordNotifications is already installed but it's out of date!"
 		Write-Output "Please try the updater script in `"$rootPath\$project`" or download from https://github.com/tigattack/$project/releases."
 	}
-}
-
-# Check user has webhook URL ready
-do {
-	$webhookPrompt = Read-Host -Prompt 'Do you have your Discord webhook URL ready? Y/N'
-}
-until ($webhookPrompt -in 'Y', 'N')
-
-# Prompt user to create webhook first if not ready
-If ($webhookPrompt -eq 'N') {
-	Write-Output 'Please create a Discord webhook before continuing.'
-	Write-Output 'Full instructions available at https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks'
-
-	# Prompt user to launch URL
-	$launchPrompt = Read-Host -Prompt 'Open URL? Y/N'
-	If ($launchPrompt -eq 'Y') {
-		Start-Process 'https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks'
-	}
-
-	exit
-}
-
-# Prompt user with config options
-do {
-	$webhookUrl = Read-Host -Prompt 'Please enter your Discord webhook URL'
-	If ($webhookUrl -notmatch $webhookRegex) {
-		Write-Output "`nInvalid webhook URL. Please try again."
-	}
-}
-until ($webhookUrl -match $webhookRegex)
-
-Write-Output "`nDo you wish to be mentioned in Discord when a job fails or finishes with warnings?"
-
-do {
-	$mentionPreference = Read-Host -Prompt "1 = No`n2 = On warn`n3 = On fail`n4 = On fail and on warn`nYour choice"
-	If (1..4 -notcontains $mentionPreference) {
-		Write-Output "`nInvalid choice. Please try again."
-	}
-}
-until (1..4 -contains $mentionPreference)
-
-If ($mentionPreference -ne 1) {
-	do {
-		try {
-			[Int64]$userId = Read-Host -Prompt "`nPlease enter your Discord user ID"
-		}
-		catch [System.Management.Automation.ArgumentTransformationMetadataException] {
-			Write-Output "`nInvalid user ID. Please try again."
-		}
-	}
-	until ($userId.ToString().Length -gt 1)
 }
 
 # Pull latest version of script from GitHub
@@ -143,39 +93,123 @@ Write-Output "Extracting files to '$rootPath'..."
 Expand-Archive -Path "$env:TEMP\$project-$release.zip" -DestinationPath "$rootPath"
 
 # Rename destination and tidy up
-Write-Output "Renaming directory and tidying up..."
+Write-Output "Renaming directory and tidying up...`n"
 Rename-Item -Path "$rootPath\$project-$release" -NewName "$project"
 Remove-Item -Path "$env:TEMP\$project-$release.zip"
 
 # Get config
 $config = Get-Content "$rootPath\$project\config\conf.json" -Raw | ConvertFrom-Json
-$config.webhook = $webhookUrl
 
-Switch ($mentionPreference) {
-	1 {
-		$config.mention_on_fail = $false
-		$config.mention_on_warning = $false
+# Check mention keys exist
+if ($null -eq $config.mention_on_warning) {
+	$mentionOnWarnExist = $false
+}
+if ($null -eq $config.mention_on_fail) {
+	$mentionOnFailExist = $false
+}
+
+# Check user has webhook URL ready
+do {
+	$webhookPrompt = Read-Host -Prompt 'Do you have your Discord webhook URL ready? Y/N'
+}
+until ($webhookPrompt -in 'Y', 'N')
+
+# Prompt user to create webhook first if not ready
+If ($webhookPrompt -eq 'N') {
+	Write-Output 'Please create a Discord webhook before continuing.'
+	Write-Output 'Full instructions available at https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks'
+
+	# Prompt user to launch URL
+	$launchPrompt = Read-Host -Prompt 'Open URL? Y/N'
+	If ($launchPrompt -eq 'Y') {
+		Start-Process 'https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks'
 	}
-	2 {
-		$config.mention_on_fail = $false
-		$config.mention_on_warning = $true
-		$config.userId = $userId
+
+	exit
+}
+
+# Prompt user with config options
+do {
+	$webhookUrl = Read-Host -Prompt 'Please enter your Discord webhook URL'
+	If ($webhookUrl -notmatch $webhookRegex) {
+		Write-Output "`nInvalid webhook URL. Please try again."
 	}
-	3 {
-		$config.mention_on_fail = $true
-		$config.mention_on_warning = $false
-		$config.userId = $userId
+}
+until ($webhookUrl -match $webhookRegex)
+
+If ($mentionOnWarnExist) {
+	Write-Output "`nDo you wish to be mentioned in Discord when a job fails or finishes with warnings?"
+
+	do {
+		$mentionPreference = Read-Host -Prompt "1 = No`n2 = On warn`n3 = On fail`n4 = On fail and on warn`nYour choice"
+		If (1..4 -notcontains $mentionPreference) {
+			Write-Output "`nInvalid choice. Please try again."
+		}
 	}
-	4 {
-		$config.mention_on_fail = $true
-		$config.mention_on_warning = $true
-		$config.userId = $userId
+	until (1..4 -contains $mentionPreference)
+
+	If ($mentionPreference -ne 1) {
+		do {
+			try {
+				[Int64]$userId = Read-Host -Prompt "`nPlease enter your Discord user ID"
+			}
+			catch [System.Management.Automation.ArgumentTransformationMetadataException] {
+				Write-Output "`nInvalid user ID. Please try again."
+			}
+		}
+		until ($userId.ToString().Length -gt 1)
+	}
+
+	# Set config values
+	$config.webhook = $webhookUrl
+	Switch ($mentionPreference) {
+		1 {
+			$config.mention_on_fail = $false
+			$config.mention_on_warning = $false
+		}
+		2 {
+			$config.mention_on_fail = $false
+			$config.mention_on_warning = $true
+			$config.userId = $userId
+		}
+		3 {
+			$config.mention_on_fail = $true
+			$config.mention_on_warning = $false
+			$config.userId = $userId
+		}
+		4 {
+			$config.mention_on_fail = $true
+			$config.mention_on_warning = $true
+			$config.userId = $userId
+		}
+	}
+}
+
+elseif ($mentionOnFailExist) {
+	do {
+		$mentionPreference = Read-Host -Prompt "Do you wish to be mentioned in Discord when a job finishes in a failed state? Y/N"
+	}
+	until ($mentionPreference -in 'Y', 'N')
+
+	# Set config values
+	If ($mentionPreference -eq 'Y') {
+		do {
+			$config.mention_on_fail = $true
+
+			try {
+				[Int64]$config.userId = Read-Host -Prompt "`nPlease enter your Discord user ID"
+			}
+			catch [System.Management.Automation.ArgumentTransformationMetadataException] {
+				Write-Output "`nInvalid user ID. Please try again."
+			}
+		}
+		until ($userId.ToString().Length -gt 1)
 	}
 }
 
 # Write config
 Try {
-	Write-Output 'Setting configuration...'
+	Write-Output "`nSetting configuration..."
 	ConvertTo-Json $config | Set-Content "$rootPath\$project\config\conf.json"
 }
 catch {
